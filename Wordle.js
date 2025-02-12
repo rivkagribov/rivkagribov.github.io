0263bd66898b28653b9c881e964471befd0c7b2c
rivkagribov <rivka.gribov@gmail.com>
2025-02-12 16:52:20 -0500

    Update Wordle.js

--- Wordle.js
+++ Wordle.js
@@ -1,4 +1,4 @@
-const wordList = ["SILLY","HEART", "CUPID", "ROSES", "STINKY", "SWEET", "DUCKS", "QUACK", "LATKE", "AMOUR", "LUCKY", "FLIRT", "FRUIT"]
+const wordList = ["SILLY","HEART", "CUPID", "ROSES", "STINK", "SWEET", "DUCKS", "QUACK", "LATKE", "AMOUR", "LUCKY", "FLIRT", "FRUIT"]
 
 let word = "", row = 1, answer = "", gameOver = false;
 let collection = document.getElementsByClassName("box");
